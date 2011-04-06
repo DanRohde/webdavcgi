565c3c5d2d98671340f43ccb3f32c23a6123e8fc
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2011-04-06 09:29:02 +0000

    + first release candidate

--- cgi-bin/webdav.pl
+++ cgi-bin/webdav.pl
@@ -23,7 +23,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
-# VERSION 0.7.0 BETA
+# VERSION 0.7.0 RC1
 # REQUIREMENTS:
 #    - see http://webdavcgi.sf.net/doc.html#requirements
 # INSTALLATION:
