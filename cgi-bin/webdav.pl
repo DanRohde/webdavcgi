4007e4a007fe9030d5cd8217874a7b7463455fbc
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2012-06-06 12:28:29 +0000

    released 0.8.3

--- cgi-bin/webdav.pl
+++ cgi-bin/webdav.pl
@@ -24,7 +24,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
-# VERSION 0.8.2-BETA
+# VERSION 0.8.3
 # REQUIREMENTS:
 #    - see http://webdavcgi.sf.net/doc.html#requirements
 # INSTALLATION:
