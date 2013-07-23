f507d2adc74fe922bd1185ab3cfc51171692bb7b
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2013-07-23 21:49:24 +0000

    - added missing copyright and license information - fixed wrong title after folder change bug

--- htdocs/views/simple/script.js
+++ htdocs/views/simple/script.js
@@ -1,3 +1,19 @@
+/*********************************************************************
+(C) ZE CMS, Humboldt-Universitaet zu Berlin
+Written 2013 by Daniel Rohde <d.rohde@cms.hu-berlin.de>
+**********************************************************************
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
 $(document).ready(function() {
 	
 	initUIEffects();
@@ -1541,6 +1557,7 @@ function updateFileList(newtarget, data) {
 			.html(response.content)
 			.attr("data-uri",newtarget);
 		$(".ajax-loader").hide();
+		$("title").html($("#titleprefix").html()+" "+newtarget.replace(/\/[^\/]+\/\.\.$/,"/"));
 		initFileList();
 		handleJSONResponse(response);
 	});
