ac9c657fca984e4c4888f50bdadd50d7f83d6b08
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2014-07-18 15:32:51 +0000

    - updated jquery and jquery-ui

--- htdocs/views/simple/contrib/minify.sh
+++ htdocs/views/simple/contrib/minify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-java -jar /etc/webdavcgi/minify/yuicompressor-2.4.2.jar $1  | gzip -c > ${1%.*}.min.${1##*.}.gz
+java -jar /etc/webdavcgi/minify/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar $1  | gzip -c > ${1%.*}.min.${1##*.}.gz
 
