93f0527b793fcf5ae99ad4a2b58891252c5c8cb2
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2014-07-28 16:01:34 +0000

    - improved treemap

--- lib/perl/WebInterface/Extension/DiskUsage.pm
+++ lib/perl/WebInterface/Extension/DiskUsage.pm
@@ -20,7 +20,8 @@
 # disable_fileaction - disables fileaction entry
 # disable_fileactionpopup - disables fileaction entry in popup menu
 # disable_apps - disables sidebar menu entry
-# 
+# timeout - timeout in seconds
+# filelimit - limits file count for treemap
 
 package WebInterface::Extension::DiskUsage;
 
@@ -58,7 +59,7 @@ sub handle {
 		return $self->handleAppsHook($$self{cgi},'listaction diskusage sel-noneormulti sel-dir disabled','du_diskusage_short','du_diskusage'); 
 	} elsif ( $hook eq 'posthandler' && $$config{cgi}->param('action') eq 'diskusage') {
 		my $suffixes = {};
-		my $content = $self->renderDiskUsage($suffixes);
+		my $content = $self->renderDiskUsage();
 		main::printCompressedHeaderAndContent('200 OK', 'text/html', $content, 'Cache-Control: no-cache, no-store');
 		return 1;
 	}
@@ -71,47 +72,63 @@ sub gs {
 	return $r;
 }
 sub renderDiskUsage {
-	my ($self, $suffixes) = @_;
+	my ($self) = @_;
 	my $text =""; 
-	my $completedu = 0;
 	my $statfstring = sprintf('%s %%d, %s %%d, %s %%d',$self->tl('statfiles'),$self->tl('statfolders'),$self->tl('statsum'));
 	my $json = new JSON();
 	my $cgi = $$self{cgi};
-	foreach my $file ($cgi->param('file')) {
-		my ($dudetails, $fcdetails) = ({},{});
-		my $filename = $file eq '' ? '.' : $file;
-		my $du = $self->getDiskUsage($main::PATH_TRANSLATED,$file,$dudetails,$fcdetails,$suffixes);
-		my @bv = $self->renderByteValue($du);
-		my %mapdata = (  id => $filename, uri=>$cgi->escape($file), size=>[0,0], children=> [ { id=>$filename, uri=>$cgi->escape($file),color=>[1,1], size=>[1,1], children=>[] } ] );
-		my $label = sprintf($self->tl('du_diskusagefor'), $filename);
-		my $dutext = "$label: " . $cgi->span({-title=>$bv[1]},$bv[0]);
-		my $fullstat = sprintf($statfstring, $$fcdetails{$file}{files}, $$fcdetails{$file}{folders}, $$fcdetails{$file}{sum});	
-		$completedu+=$du;
+	my $counter = { start => time() };
+	my %mapdata = (  id => $main::REQUEST_URI, uri=>$cgi->escape($main::REQUEST_URI), children=> [] );
+	
+	my $cc = 0;
+	my $ccst = 1/6;
 
-		if (keys %{$dudetails} > 0) {
-			my $details = "";
-			$details .= $cgi->div($fullstat); 
-			$details .= $cgi->start_table({-class=>'diskusage details'});
-			$details .= $cgi->Tr({},$cgi->th({-class=>'diskusage filename'},$self->tl('name')).$cgi->th({-class=>'diskusage size',-title=>$bv[1]},$self->tl('size')));
-			foreach my $p (sort { $$dudetails{$b} <=> $$dudetails{$a} || $b cmp $a } keys %{$dudetails}) {
-				my @pbv = $self->renderByteValue($$dudetails{$p});
-				my $perc =  $du > 0 ? 100*$$dudetails{$p}/$du : 0;
-				my $percsum = $$fcdetails{$file}{sum} >0 ? $$fcdetails{$p}{sum}/$$fcdetails{$file}{sum} : 0;
-				my $uri = $self->getURI($p);
-				my $title = sprintf('%.2f%%, '.$statfstring,$perc,$$fcdetails{$p}{files},$$fcdetails{$p}{folders},$$fcdetails{$p}{sum});
-				
-				push @{$mapdata{children}[0]{children}}, { uri=>$uri, title=>$title, val=>$pbv[0], id=>$p, size=>[gs($perc/100),gs($percsum)],color=>[gs($perc/100),gs($percsum)]};
-				
-				$details.=$cgi->Tr({-class=>'diskusage entry'}, 
-					$cgi->td({-class=>'diskusage filename',-title=>$title},
-						$cgi->div({-class=>'diskusage perc',-style=>sprintf('width: %.0f%%;',$perc)},
-							$cgi->a({-class=>'action changeuri',-href=>$main::REQUEST_URI.$uri},$cgi->escapeHTML($p))))
-					.$cgi->td({-title=>$pbv[1], -class=>'diskusage size'}, $pbv[0]));
-			}
-			$details.=$cgi->end_table();
-			$dutext.=$cgi->div({-class=>'diskusage accordion'}, $cgi->h3($self->tl('du_details')).$cgi->div($details));
+
+	foreach my $file ($cgi->param('file')) {
+		$self->getDiskUsage($main::PATH_TRANSLATED,$file,$counter);		
+	}	
+	my @pbvsum = $self->renderByteValue($$counter{size}{all});
+	my $filenamelist = join(', ',$cgi->param('file'));
+	$filenamelist='.' if $filenamelist eq "";
+	$filenamelist=substr($filenamelist,0,100).'...' if length($filenamelist)>100;
+	$text.= $cgi->div({-title=>$pbvsum[1]}, sprintf($self->tl('du_diskusageof'), $filenamelist). ': '.$pbvsum[0])
+		.$cgi->div(sprintf($statfstring,$$counter{count}{all}{files},$$counter{count}{all}{folders},$$counter{count}{all}{sum}));
+	
+	my $table = $cgi->start_table({-class=>'diskusage details'})
+			.$cgi->Tr({},$cgi->th({-class=>'diskusage filename'},$self->tl('name')).$cgi->th({-class=>'diskusage size', -title=>($self->renderByteValue($$counter{size}{all}))[1]},$self->tl('size')));
+	foreach my $folder (sort {$$counter{size}{path}{$b} <=> $$counter{size}{path}{$a} || $a cmp $b} keys %{$$counter{size}{path}}) {
+		my $perc = 100*$$counter{size}{path}{$folder}/$$counter{size}{all};
+		my $title = sprintf('%.2f%%, '.$statfstring,$perc,$$counter{count}{files}{$folder},$$counter{count}{folders}{$folder},$$counter{count}{sum}{$folder});
+		my @pbv = $self->renderByteValue($$counter{size}{path}{$folder});
+		my $foldername = $folder;
+		$foldername=~s/^\Q$main::REQUEST_URI\E//;
+		$foldername='./' if $foldername eq "";
+		my $uri = $self->getURI($foldername);
+		$table.= $cgi->Tr({-class=>'diskusage entry'},
+			$cgi->td({-class=>'diskusage filename',-title=>$title}, 
+				$cgi->div({-class=>'diskusage perc',-style=>sprintf('width: %.0f%%;',$perc)},
+							$cgi->a({-class=>'action changeuri',-href=>$main::REQUEST_URI.$uri},$cgi->escapeHTML($foldername))))
+			.$cgi->td({-title=>$pbv[1], -class=>'diskusage size'}, $pbv[0])
+		);
+		
+		my $files = $$counter{size}{files}{$folder};
+		my @childmapdata = ();
+		foreach my $file ( keys %{$files} ) {
+			my @pbvfile = $self->renderByteValue($$files{$file});
+			my $perc = $$counter{size}{path}{$folder} > 0 ? $$files{$file} / $$counter{size}{path}{$folder} : 0;
 			
-			$dutext.=$cgi->div({-class=>'diskusage treemap accordion'}, $cgi->h3($self->tl('du_treemap')).
+			my $uri = $self->getURI($foldername);
+			push @childmapdata, { uri=>$uri, title=>"<br/>$foldername: $pbv[0] $title", val=>$pbvfile[0], id=>$file, size=>[gs($perc),gs($perc)],color=>[gs($cc),gs($cc)]};
+			
+			#last if scalar(@childmapdata) >= $self->config('filelimit',50);
+		}
+		my $perccount = $$counter{count}{all}{files} >0 ? $$counter{count}{files}{$folder} / $$counter{count}{all}{files} : 0;
+		push @{$mapdata{children}}, { id=>$foldername, uri=>$uri,color=>[$cc,$cc], size=>[gs($$counter{size}{path}{$folder}/$$counter{size}{all}), gs($perccount)], children=>\@childmapdata };
+		$cc = ($cc+$ccst >1) ? 0 : $cc+$ccst;
+	}
+	$table.=$cgi->end_table();
+	$text.= $cgi->div({-class=>'diskusage accordion'}, $cgi->h3($self->tl('du_details')) . $cgi->div($table));
+	$text.=$cgi->div({-class=>'diskusage treemap accordion'}, $cgi->h3($self->tl('du_treemap')).
 					$cgi->div( 
 					  $cgi->div({-class=>'treemappanel',-data_mapdata=>$json->encode(\%mapdata)})
 					. $cgi->div({-class=>'diskusage treemap switch'},
@@ -119,43 +136,65 @@ sub renderDiskUsage {
 						.$cgi->div({-class=>'diskusage treemap byfilecount'}, $self->tl('du_treemap_byfilecount'))
 					))
 						);
-		} else {
-			$dutext .= $cgi->div( $fullstat );
-		}
-		$text .= $cgi->div($dutext);
-	}
-	return $cgi->div({-title=>$self->tl('du_diskusage').': '.($self->renderByteValue($completedu))[0]},$text);
+	#$text.=$self->renderStatistics();
+	return $cgi->div({-title=>$self->tl('du_diskusage').': '.($self->renderByteValue($$counter{size}{all}))[0]}, $text);
+}
+sub renderStatistics {
+	my ($self) = @_;
+	my $cgi = $$self{cgi};
+	my $content = $cgi->h3($self->tl('du_statistics'));
+	
+	
+	return $cgi->div({-class=>'diskusage statistics accordion'}, $content);;
 }
 sub getURI { 
 	my ($self, $relpath) = @_;
 	return join('/',map({ $$self{cgi}->escape($_) } split(/\//,$relpath)));
 }
+
 sub getDiskUsage {
-	my ($self,$path,$file,$sizes,$fcounts,$suffixes) = @_;
+	my ($self, $path, $file, $counter) = @_;
+	
 	my $backend = $$self{backend};
-	my $size = 0;
-	foreach my $f (@{$$self{backend}->readDir("$path$file")}) {
-		my $nf = "$file$f";
-		my $np = "$path$nf";
-		if ($backend->isDir($np)&&!$backend->isLink($np)) {
-			$nf.='/';
-			$$sizes{$nf}=$self->getDiskUsage($path,$nf,$sizes,$fcounts,$suffixes); ## + ($backend->stat("$path$nf"))[7]; # folders have sometimes a size but is not relevant
-			$size+=$$sizes{$nf};
-			$$fcounts{$file}{folders}++;
-			$$fcounts{$file}{folders}+=$$fcounts{$nf}{folders};
-			$$fcounts{$file}{files}+=$$fcounts{$nf}{files};
-			$$fcounts{$file}{sum}+=$$fcounts{$nf}{sum};
-		} else {
-			my $fsize = ($backend->stat($np))[7];
-			$size+=$fsize;
-			$$fcounts{$file}{files}++;
-			if ($f=~/(\.[^.]+)$/) {
-				$$suffixes{count}{$1}++;
-				$$suffixes{sizes}{$1}+=$fsize;
-			};
+	
+	
+	$file=~s/^\///;
+	
+	return if time() - $$counter{start} > $self->config('timeout',60); 
+	
+	$$counter{count}{all}{sum}++;	
+	$$counter{count}{sum}{$path}++;
+	$$counter{count}{subdir}{sum}{$path}{$file}++ if $file ne "";
+	
+	
+	my $full = $path.$file;
+	if ($backend->isDir($full) && !$backend->isLink($full)) {
+		$$counter{count}{all}{folders}++;
+		$$counter{count}{folders}{$path}++; 
+		
+		#$file.='/' unless $file=~/\/$/;
+		foreach my $f (@{$backend->readDir($full)}) {
+			$f.='/' if $backend->isDir($full.$f);
+			$self->getDiskUsage($full,$f,$counter);
 		}
-		$$fcounts{$file}{sum}++;
+	} else {
+		my $fs  = ($$self{backend}->stat($path.$file))[7];		
+		$$counter{count}{all}{files}++;
+		$$counter{count}{files}{$path}++;
+		
+		$$counter{size}{all}+=$fs; 
+		$$counter{size}{path}{$path}+=$fs; 
+		$$counter{size}{files}{$path}{$file eq "" ? '.' : $file}+=$fs;
+		
+		
+		if ($file=~/(\.[^.]+)$/) {
+			$$counter{suffixes}{all}{size}{$1}++;
+			$$counter{suffixes}{size}{$path}{$1}+=$fs;
+			
+			$$counter{suffixes}{all}{count}{$1}++;
+			$$counter{suffixes}{count}{$path}{$1}{count}++;
+		}
 	}
-	return $size;
+	
 }
 1;
