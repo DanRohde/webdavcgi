db312eba4436452b6cd60ea96196a25927ba78a6
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2015-06-19 14:40:10 +0000

    - forgot version number

--- htdocs/views/simple/contrib/minify.sh
+++ htdocs/views/simple/contrib/minify.sh
@@ -2,7 +2,7 @@
 
 while (( "$#" )) ; do
 	echo $1
-java -jar /etc/webdavcgi/minify/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar $1  | gzip -c > ${1%.*}.min.${1##*.}.gz
+java -jar /etc/webdavcgi/minify/yuicompressor-2.4.8.jar $1  | gzip -c > ${1%.*}.min.${1##*.}.gz
 shift
 done
 
