40b33fa59fb6f49194431c4965a8d84ba8f464e3
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2015-10-21 08:40:58 +0000

    - fixed HexDump extension dialog resize bug

--- lib/perl/WebInterface/Extension/HexDump.pm
+++ lib/perl/WebInterface/Extension/HexDump.pm
@@ -46,7 +46,7 @@ sub handle {
 		$ret =  { action=>'hexdump', label=>'hexdump',  classes=>'access-readable', type=>'li' };
 	} elsif ($hook eq 'gethandler' && $cgi->param('action') eq 'hexdump') {
 		my $content = $cgi->div({title=>$self->tl('hexdump')},
-			$cgi->div($self->quoteWhiteSpaces($cgi->escapeHTML(scalar $cgi->param('file')))) 
+			$cgi->div({class=>'hexdump filename'},$self->quoteWhiteSpaces($cgi->escapeHTML(scalar $cgi->param('file')))) 
 			.$cgi->pre({class=>'hexdump'}, $cgi->escapeHTML($self->renderHexDump(scalar $cgi->param('file')))));
 		main::printCompressedHeaderAndContent('200 OK','text/html', $content, 'Cache-Control: no-cache, no-store');
 		$ret = 1;
