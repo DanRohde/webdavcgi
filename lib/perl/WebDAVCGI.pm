2c159029d7a75a6dd24703e6d76f580fcfb14474
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-05-24 07:07:57 +0000

    - fixed enter doesn't work for feedback button bug

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.1BETA20160524.1';
+$RELEASE = '1.1.1BETA20160524.2';
 
 use vars qw( $_METHODS_RX );
 
