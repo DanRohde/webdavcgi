0cf56506b8fca8755e00f0e84b2fcf3610a3335c
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-07-15 17:28:54 +0000

    - simplified markdown list rendering - fixed some markdown rendering bugs

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.1BETA20160715.3';
+$RELEASE = '1.1.1BETA20160715.4';
 
 use vars qw( $_METHODS_RX );
 
