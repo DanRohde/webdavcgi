9417f0e07fe7ab787a6578f0a59461d5ea5bd4b9
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-07-20 13:09:01 +0000

    - fixed some fullscreen bugs related to markdown editor in TextEditor extension

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.1BETA20160720.1';
+$RELEASE = '1.1.1BETA20160720.3';
 
 use vars qw( $_METHODS_RX );
 
