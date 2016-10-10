9985e68f3c8eb28558de4d1b7e6cbfcea113f3de
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-10-10 14:35:58 +0200

    - started new beta

--- dist.sh
+++ dist.sh
@@ -9,3 +9,5 @@ svn export svn://svn.code.sf.net/p/webdavcgi/code/trunk ${TMPPATH}
 
 (cd `dirname ${TMPPATH}`; zip -r  webdavcgi-${RELEASE}.zip `basename ${TMPPATH}`)
 (cd `dirname ${TMPPATH}`; tar jcf webdavcgi-${RELEASE}.tar.bz2 `basename ${TMPPATH}`)
+(cd $(dirname ${TMPPATH}); cp webdavcgi-${RELEASE}.zip  webdavcgi-latest.zip)
+(cd $(dirname ${TMPPATH}); cp webdavcgi-${RELEASE}.tar.bz2 webdavcgi-latest.tar.bz2 )
