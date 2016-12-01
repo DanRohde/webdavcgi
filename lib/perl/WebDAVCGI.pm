094f5e86a8bd1906eb6fbfba6f68106440963451
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-12-01 18:35:57 +0100

    - fixed some focus/hover style bugs

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.2BETA20161129.1';
+$RELEASE = '1.1.2BETA20161201.1';
 
 use vars qw( $_METHODS_RX );
 
