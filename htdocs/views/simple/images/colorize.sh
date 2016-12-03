b576c18f66de5abd56a60cfa14d06e11c5107db4
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-12-03 12:45:27 +0100

    - fixed wrong cursor on action buttons bug - improved helper scripts

--- htdocs/views/simple/images/colorize.sh
+++ htdocs/views/simple/images/colorize.sh
@@ -4,25 +4,15 @@
 FILES="about.png addbookmark.png bookmarks.png copy.png cut.png delete.png download.png edit.png group.png paste.png refresh.png remove.png rename.png upload.png user-edit.png user-properties.png view-filter.png cancel.png createfolder.png createfile.png createlink.png changedir.png toggleselection.png logout.png help.png home.png add.png search.png settings.png refresh-start.png refresh-pause.png refresh-stop.png selectall.png selectnone.png contact.png size.png sigma.png fullscreen-on.png fullscreen-off.png bookmark.png uploaddir.png fileactions.png save.png"
 
 test "$#" = "0" && exit 1
-set=$1
-base=$2
-hover=$3
+set1=set-$1
+set2=set-$2
+from=$3
+to=$4
 
-cd set-$set
-
-FILESWITHNULL="null: $(echo $FILES | sed -e 's@ @ null: @g')"
-# with shadows:
-#montage $FILESWITHNULL -tile 1x -shadow -background none -geometry 16x16+0+0 sprite.png
-# without shadows:
-echo montage $FILESWITHNULL -tile 1x -background none -geometry 16x16+2+2 -quality 100 sprite.png
-montage $FILESWITHNULL -tile 1x -background none -geometry 16x16+2+2 -quality 100  sprite.png
+echo "set1=$set1  set2=$set2 from=$from to=$to"
 
 i=1
 for f in $FILES ; do
-	echo $f -$(( $i * 40 - 25))
-	i=$(( $i + 1 ))
+    convert ${set1}/$f -fill "${to:=#000000}" -opaque "${from:=#808080}" ${set2}/$f
 done
 
-convert sprite.png -fill "${hover:=#000000}" -opaque "${base:=#808080}" sprite-hover.png
-
-mv sprite.png sprite-hover.png ..
