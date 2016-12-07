5946e7fc4912baf0781be853e16c3d86bd53331c
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-12-07 10:17:47 +0100

    - fixed view filter bug - improved color selction in Highlighter extensino - added missing CSS classes

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.2BETA20161204.6';
+$RELEASE = '1.1.2BETA20161206.1';
 
 use vars qw( $_METHODS_RX );
 
