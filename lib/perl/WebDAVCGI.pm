f967c7ab75b11ce45e1d292f9e34a2d926f85ddf
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2016-12-28 14:32:35 +0100

    - blowed up user guide - fixed small style bugs

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -55,7 +55,7 @@ use Backend::Manager;
 use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
-$RELEASE = '1.1.2BETA20161228.2';
+$RELEASE = '1.1.2BETA20161228.3';
 
 use vars qw( $_METHODS_RX );
 
