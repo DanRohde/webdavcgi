eb75b6e28722f7c611b5d25eeb7cfbee80aa6f67
Daniel Rohde <d.rohde@cms.hu-berlin.de>
2017-02-08 18:28:18 +0100

    - added SVG icons to Search and Dowload extension

--- lib/perl/WebDAVCGI.pm
+++ lib/perl/WebDAVCGI.pm
@@ -57,7 +57,7 @@ use HTTPHelper qw( print_header_and_content );
 use CacheManager;
 
 
-$RELEASE = '1.1.2BETA20170208.1';
+$RELEASE = '1.1.2BETA20170208.2';
 
 use vars qw( $_METHODS_RX );
 
--- lib/perl/WebInterface/Extension/createhovericons.sh
+++ lib/perl/WebInterface/Extension/createhovericons.sh
@@ -9,3 +9,12 @@ for i in */htdocs/*.png */htdocs/images/*.png ; do
 	test "$i" -nt "$ti" && convert "$i" -fill "#000000" -opaque "#808080" $ti
 
 done
+for i in */htdocs/*.svg */htdocs/images/*.svg ; do
+    [[ $i =~ -hover.svg ]] &&  continue
+
+    su=${i##*.}
+    ti=${i%%.*}-hover.$su
+
+    test "$i" -nt "$ti" && sed -e 's/#808080/#000000/g' "$i" > $ti
+        
+done
