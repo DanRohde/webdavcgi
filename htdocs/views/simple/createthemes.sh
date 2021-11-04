737adbfb26d76b61cdba19c6527e5534dcf859d8
Daniel Rohde <rohdedan@gimli.cms.hu-berlin.de>
2021-11-04 10:15:26 +0100

    - fixed some style problems

--- htdocs/views/simple/createthemes.sh
+++ htdocs/views/simple/createthemes.sh
@@ -5,7 +5,7 @@ for t in "${BP}"/less/themes/*.less ; do
 	BN=$(basename $t .less)
 	f="${TP}"/"${BN}".min.css
     echo Create theme $BN.
-	lessc - --include-path="${BP}"/less:"${BP}"/less/themes < "${t}" | yuglify --terminal --type css > "${f}"
+	lessc - --include-path="${BP}"/less:"${BP}"/less/themes < "${t}" | node_modules/yuglify/bin/yuglify --terminal --type css > "${f}"
 	brotli < "${f}" > "${f}.br"
 	gzip -f "${f}"
 done
